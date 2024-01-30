272299da6ee61637465170b618a68717dbd40de8
felixyz789 <mattiafelix0@gmail.com>
2024-01-30 11:51:56 +0000

    2660 added

--- Leetcode/914. X of a Kind in a Deck of Cards/Python/Solution_1.py
+++ Leetcode/914. X of a Kind in a Deck of Cards/Python/Solution_1.py
@@ -1,12 +1,5 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        nums_map = defaultdict(int)
-
-        for c in deck:
-            nums_map[c] += 1
-
-        all =  [nums_map[k] for k in nums_map]
-        
-        result = math.gcd(*all)
-
-        return True if result > 1 else False        
+        nums_map = Counter(deck)
+        result = math.gcd(*nums_map.values())
+        return result > 1
