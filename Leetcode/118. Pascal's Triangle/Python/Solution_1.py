3347a055d7e56eded074c754c6e9406719128eab
felixyz789 <mattiafelix0@gmail.com>
2024-01-31 19:27:43 +0000

    118 modified

--- Leetcode/118. Pascal's Triangle/Python/Solution_1.py
+++ Leetcode/118. Pascal's Triangle/Python/Solution_1.py
@@ -3,13 +3,10 @@ class Solution:
         solution = [[] for _ in range(numRows)]
 
         for i in range(numRows):
-            if i == 0:
-                solution[i].append(1)
-            else:
-                for j in range(i + 1):
-                    if j - 1 > -1 and j != i :
-                        solution[i].append(solution[i - 1][j - 1] + solution[i - 1][j])
-                    else:
-                        solution[i].append(1)
+            for j in range(i + 1):
+                if j - 1 > -1 and j != i :
+                    solution[i].append(solution[i - 1][j - 1] + solution[i - 1][j])
+                else:
+                    solution[i].append(1)
         
         return solution
